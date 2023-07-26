4bd28b6703786d21e4afed6e8580da12a1d5ce61
Paweł Jonik <p.jonik2005@gmail.com>
2023-07-27 01:16:12 +0200

    v1.2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double obecneSaldo = 0;
-        System.out.println("Twoje saldo w banku wynosi: " + obecneSaldo);
+        System.out.println("Twoje saldo w banku wynosi: " + obecneSaldo+ "zł");
         while (true) {
             System.out.println("Wybierz typ operacji: ");
             System.out.println("0. Wyjdź z bankomatu");
@@ -16,16 +16,14 @@ public class Main {
                 case 1:
                     System.out.println("Podaj jaką kwotę chcesz wpłacić: ");
                     double kwota1 = scanner.nextDouble();
-                    //double saldoPoOperacji1 = obecneSaldo + kwota1;
                     obecneSaldo += kwota1;
-                    System.out.println("Saldo po wpłacie: " + obecneSaldo);
+                    System.out.println("Saldo po wpłacie: " + obecneSaldo + "zł");
                     continue;
                 case 2:
                     System.out.println("Podaj jaką kwotę chcesz wypłacić: ");
                     double kwota2 = scanner.nextDouble();
-                    //double saldoPoOperacji2 = obecneSaldo - kwota2;
                     obecneSaldo -= kwota2;
-                    System.out.println("Saldo po wpłacie: " + obecneSaldo);
+                    System.out.println("Saldo po wpłacie: " + obecneSaldo+ "zł");
                     continue;
             }
             if (opcja == 0) {
